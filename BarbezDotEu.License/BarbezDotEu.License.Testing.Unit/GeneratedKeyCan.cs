f5bf332ea9fd58c7e61abf55c3317c967cb110ef
Hannes Barbez <hannesbarbez@hotmail.com>
2024-01-02 17:51:54 +0100

    Revertible

--- BarbezDotEu.License/BarbezDotEu.License.Testing.Unit/GeneratedKeyCan.cs
+++ BarbezDotEu.License/BarbezDotEu.License.Testing.Unit/GeneratedKeyCan.cs
@@ -2,6 +2,7 @@
 // Licensed under the GNU General Public License v3.0
 
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using BarbezDotEu.License.Generation;
@@ -22,9 +23,12 @@ namespace BarbezDotEu.License.Testing.Unit
         public void BeVerified()
         {
             // Check numberOfKeys matches
-            var numberOfKeys = 10000;
+            var numberOfKeys = 30000;
             var excludedKeys = Array.Empty<string>();
+            var watch = Stopwatch.StartNew();
             var keys = KeyGenerator.GenerateKeys((uint)numberOfKeys, excludedKeys);
+            watch.Stop();
+            Console.WriteLine($"{numberOfKeys} keys in {watch.ElapsedMilliseconds / 1000} seconds.");
             Assert.AreEqual(numberOfKeys, keys.Count());
 
             // Check keys can be verified
